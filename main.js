b0e51342adfedfcbde259237e9051d79030be483
Muntasir Biojid <muntasir2165@hotmail.com>
2020-06-15 21:33:50 -0700

    v-39 mainwindow class

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const { app, BrowserWindow, Menu, ipcMain, Tray } = require('electron');
+const { app, Menu, ipcMain, Tray } = require('electron');
 const log = require('electron-log');
 const Store = require('./Store');
-
+const MainWindow = require('./MainWindow');
 // Set env
 process.env.NODE_ENV = 'development';
 // process.env.NODE_ENV = 'production';
@@ -25,24 +25,7 @@ const store = new Store({
 });
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    title: 'SysTop',
-    width: isDev ? 800 : 355,
-    height: 500,
-    icon: './assets/icons/icon.png',
-    resizable: isDev ? true : false,
-    show: false,
-    opacity: 0.9,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
-  }
-
-  mainWindow.loadFile('./app/index.html');
+  mainWindow = new MainWindow('./app/index.html', isDev);
 }
 
 app.on('ready', () => {
